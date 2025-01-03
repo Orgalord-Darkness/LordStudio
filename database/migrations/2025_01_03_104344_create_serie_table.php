3a12a773a0f87fdd09dd6a5b3598b3909cf3053e
Orgalord-Darkness <heddy.mameri@gmail.com>
2025-01-03 11:55:54 +0100

    Création de la table série

---
+++ database/migrations/2025_01_03_104344_create_serie_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSerieTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('serie', function (Blueprint $table) {
+            $table->id('id_serie');
+            $table->string('nom'); 
+            $table->string('synopsis') ;
+            $table->foreignId('id_categorie')->constraint('categorie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('serie');
+    }
+}
